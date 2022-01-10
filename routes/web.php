83672941f9752843da168b6668ba1c380e37db80
rafeee.ir <rafeee.ir@gmail.com>
2022-01-10 10:56:25 +0330

    before vue

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 Route::group(['middleware' => ['auth']], function () {
 
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/activities', [App\Http\Controllers\LogController::class, 'index'])->name('activities');
     Route::get('/activities/me', [App\Http\Controllers\LogController::class, 'myActivities'])->name('myActivities');
     Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users');
